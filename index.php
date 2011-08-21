81fecf2eed24345cf7c2c3a04e613e0d6a2c6fc5
Stanford Rosenthal <nolastan@gmail.com>
2011-08-21 16:44:31 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+define('PROJECT_NAME', 'Stanford Rosenthal');
+define('COMPANY_NAME', 'rosenthal.co');
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="reset.css" />
+	<link rel="stylesheet" href="one-page.css" />
+	<title>Stanford Rosenthal</title>
+</head>
+<body>
+<div id="content">
+<header class="clearfix">
+	<h1 id="project_name"><?=PROJECT_NAME?></h1>
+	<h2 id="company_name"><?=COMPANY_NAME?></h2>
+</header>
+<h3 id="project_description">I'm a <a href="http://wustl.edu">Wash U</a> student studying computer science and visual design.</h3>
+<p class="links">
+<a href="http://twitter.com/nolastan" title="Follow me on Twitter"><img src="images/twitter-2.png" alt="Twitter" /></a>
+<a href="http://www.linkedin.com/in/stanfordrosenthal"><img src="images/linkedin.png" alt="Twitter" /></a>
+<a href="http://github.com/nolastan" title="See my activity on Github"><img src="images/github.png" alt="Github" /></a>
+<a href="http://www.flickr.com/photos/nolastan/" title="See my photos on Flickr"><img src="images/flickr.png" alt="Flickr" /></a>
+<a href="http://delicious.com/standaman2009" title="View my links on Delicious"><img src="images/delicious.png" alt="Del.icio.us" /></a>
+</p>
+
+<p>Right now, I'm working on changing the way <strong>custom t-shirts</strong> are ordered. We're using Ruby on Rails and jQuery, and our team is awesome.</p>
+
+<p>In the meantime, I'm doing some <strong>freelance work</strong> to pay the bills. If you're looking for a web developer-designer, you should <a href="mailto:stan@rosenthal.co">email me</a>.</p>
+
+<p>Got a big idea? Looking to meet like-minded students? Need to hire a genius?  Check out <strong>Wash U Tech Entrepreneurs</strong> at <a href="http://wute.wustl.edu">wute.wustl.edu</a>.</p>
+
+
+<ul class="action_buttons">
+	<li><a href="resume.pdf" class="large green button" >Download my resume</a></li>
+</ul>
+
+
+
+
+</div>
+<footer>
+<p>I eat buzzwords for breakfast.</p>
+</footer>
+
+</body>
+</html>
